133ca2aaefcb0c16adbe5e01eeeaf4ea638348f6
zeyad_sofy <zeyadsofy000@gmail.com>
2024-05-17 16:54:37 +0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SuperAdminController;
+use App\Http\Controllers\SubAdminController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/register',[AuthController::class,'loadRegister']);
+Route::post('/register',[AuthController::class,'register'])->name('register');
+
+Route::get('/login',[AuthController::class,'loadLogin']);
+Route::post('/login',[AuthController::class,'login'])->name('login');
+Route::get('/logout',[AuthController::class,'logout']);
+
+// ********** Super Admin Routes *********
+Route::group(['prefix' => 'super-admin','middleware'=>['web','isSuperAdmin']],function(){
+    Route::get('/dashboard',[SuperAdminController::class,'dashboard']);
+
+    Route::get('/users',[SuperAdminController::class,'users'])->name('superAdminUsers');
+    Route::get('/manage-role',[SuperAdminController::class,'manageRole'])->name('manageRole');
+    Route::post('/update-role',[SuperAdminController::class,'updateRole'])->name('updateRole');
+});
+
+// ********** Sub Admin Routes *********
+Route::group(['prefix' => 'sub-admin','middleware'=>['web','isSubAdmin']],function(){
+    Route::get('/dashboard',[SubAdminController::class,'dashboard']);
+});
+
+// ********** Admin Routes *********
+Route::group(['prefix' => 'admin','middleware'=>['web','isAdmin']],function(){
+    Route::get('/dashboard',[AdminController::class,'dashboard']);
+});
+
+// ********** User Routes *********
+Route::group(['middleware'=>['web','isUser']],function(){
+    Route::get('/dashboard',[UserController::class,'dashboard']);
+});
